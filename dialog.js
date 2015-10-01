08a88c22ff4b2b579be68d300cf7db4bbadb2dc8
Eirik Arnold <eiriktf2@gmail.com>
2015-10-01 13:50:05 -0400

    More delves into jquery and picker reworking
    the basis for the random events is being made with the rework of the
    choice system, more organization of the code.

--- dialog.js
+++ dialog.js
@@ -15,15 +15,17 @@ var health = 100;
 
 hostileEncounter = true;
 
-//Inventory variable
+
+//Inventory BEGIN\\
 var Equipped = new Array();
 var Inventory = new Map();
 var InvObj = {},
     invFunc = function() {
 };
 InvKeyString = "Inventory";
-//more inventory (probably should be a map but for now its just a list of variables.)
 
+
+//Inv items parcer
 var invXML = new XMLHttpRequest();
 invXML.open("GET", "EnemyNodes.xml", true);
 invXML.onload = function(e) {
@@ -48,6 +50,8 @@ invXML.onerror = function(e) {
 };
 invXML.send();
 
+
+// main inv system
 var head;
 var shoulders;
 var upperB;
@@ -57,14 +61,19 @@ var hands;
 var misc;
 var knap;
 
-var HDis = document.getElementById("HeadValue");
-var SDis = document.getElementById("ShoulderValue");
-var UDis = document.getElementById("UpperValue");
-var LDis = document.getElementById("LowerValue");
-var LeDis = document.getElementById("HandValue");
-var HaDis = document.getElementById("LegValue");
-var MDis = document.getElementById("KnapValue");
-var KDis = document.getElementById("MiscValue");
+$("#HeadValue").hide();
+$("#ShoulderValue").hide();
+$("#UpperValue").hide();
+$("#LowerValue").hide();
+$("#HandValue").hide();
+$("#LegValue").hide();
+$("#KnapValue").hide();
+$("#MiscValue").hide();
+
+
+//END INVENTORY SYSTEM\\
+
+
 //Health and conditions (IE food drink, injury system!)
 var health = 100;
 
@@ -78,47 +87,31 @@ var intellegence = Skills[3];
 var wisdom = Skills[4];
 var dexterity = Skills[5];
 
-//chain runs
-var firstChainRun = false;
-var secondChainRun = false;
-var thirdChainRun = false;
-//other
+$("#str").text(strength);
+$("#end").text(endurance);
+$("#cha").text(charisma);
+$("#int").text(intellegence);
+$("#wis").text(wisdom);
+$("#dex").text(dexterity);
 
-//var dci = document.getElementById("continueBtn");
-//var nami = document.getElementById("mainbutton");
-//var kami = document.getElementById("equipbtn");
-$("#datmutton").hide();
-$("#mainbutton").hide();
+
+
+
+//Buttons
+$("#continueBtn").hide();
+$("#mainContinue").hide();
 $("#eauipbtn").hide();
 $("#closer").hide();
 $("#hostileDongle").hide();
-//var Norgami = document.getElementById("HostileDongle");
+//Choice Buttons
+$("#btnA").show();
+$("#btnB").show();
+$("#btnC").show();
+$("#btnD").show();
 
-//kami.style.display = "none";
-//dci.style.display = "";
-//nami.style.display = "none";
-//Norgami.style.display = "none";
 
-var btnA = document.getElementById("btnA");
-var btnB = document.getElementById("btnB");
-var btnC = document.getElementById("btnC");
-var btnD = document.getElementById("btnD");
-//var kami = document.getElementById("invbtn");
-
-var goblinStatus = true;
-var returner = false;
-var currentDesc;
-var theta = false;
-
-var StartArea = false;
-
-var Swp1 = false;
-var swordget = false;
-
-//chrome --allow-file-access-from-files
 
 //XML Parser
-//var xhr = "C:/Users/Student/Documents/AptanaStudio3Workspace/TheFirstLegend/theBombRange/RoomNodes.xml";
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "RoomNodes.xml", true);
 xhr.onload = function() {
@@ -146,6 +139,8 @@ xhr.onerror = function(e) {
 };
 xhr.send();
 
+
+
 //log
 function log(msg, clear) {
 	var log = document.getElementById('log');
@@ -160,6 +155,8 @@ function log(msg, clear) {
 		log.innerHTML = msg + "\n" + log.innerHTML;
 	}
 };
+//END SETUP\\
+
 
 //starter
 function startadv() {
@@ -172,116 +169,21 @@ function startadv() {
 	}
 }
 
-//skill display
-//kami.style.display = "none";
-//dci.style.display = "none";
-//nami.style.display = "none";
-//Norgami.style.display = "none";
-
-
-
-//document.getElementById("health").innerHTML = health;
-
-//var str = $("#str").html();
-$("#str").text(strength);
-$("#end").text(endurance);
-$("#cha").text(charisma);
-$("#int").text(intellegence);
-$("#wis").text(wisdom);
-$("#dex").text(dexterity);
-//document.getElementById("end").innerHTML = endurance;
-//document.getElementById("cha").innerHTML = charisma;
-//document.getElementById("int").innerHTML = intellegence;
-//document.getElementById("wis").innerHTML = wisdom;
-//document.getElementById("dex").innerHTML = dexterity;
-
-
-//chain runs
-function firstChain(evt) {
-
-	document.getElementById("choiceA").innerHTML = currentDesc.getElementsByTagName("startA")[0].childNodes[0].nodeValue + "<br>";
-	document.getElementById("choiceB").innerHTML = currentDesc.getElementsByTagName("startA")[1].childNodes[0].nodeValue + "<br>";
-	document.getElementById("choiceC").innerHTML = currentDesc.getElementsByTagName("startA")[2].childNodes[0].nodeValue + "<br>";
-	document.getElementById("choiceD").innerHTML = currentDesc.getElementsByTagName("startA")[3].childNodes[0].nodeValue + "<br>";
-	document.getElementById("log").innerHTML = currentDesc.getElementsByTagName("StartOpDesc")[0].childNodes[0].nodeValue;
-	firstChainRun = true;
+//BEGINS THE CHOICE ENGINE\\
+var wheelLands = Math.floor((Math.random() * 100) + 1);
+console.log(wheelLands);
+var outcomes = {
+	
+	
+	
 }
 
-//inventory functions
-function InventoryCheck() {
-	Tsugami.style.display = "";
-	document.getElementById("inventory").innerHTML = Inventory.get(InvKeyString);
 
-}
 
-function EquipmentCheck() {
-	Norgami.style.display = "";
 
-}
 
-function closeFunction() {
-	Tsugami.style.display = "none";
-	document.getElementById("inventory").innerHTML = "";
-}
 
-//changers
-function changeForA() {
-	if (firstChainRun == true && a == false) {
-		log("You have no idea where Koth could be, so you do nothing!" + "<br>");
-		//log("", true);
-		a = true;
-
-	}
-	if (swordget == false && Swp1 == true) {
-		log("", true);
-		btnA.style.display = "none";
-		btnB.style.display = "none";
-		btnC.style.display = "none";
-		btnD.style.display = "none";
-		nami.style.display = "";
-		document.getElementById("choiceA").innerHTML = "";
-		document.getElementById("choiceB").innerHTML = "";
-		document.getElementById("choiceC").innerHTML = "";
-		document.getElementById("choiceD").innerHTML = "";
-		document.getElementById("log").innerHTML = "";
-		document.getElementById("log").innerHTML = currentDesc.getElementsByTagName("swampOneAA")[0].childNodes[0].nodeValue;
-		swordget = true;
-		console.log(endurance);
-
-	}
-}
-
-function changeForB() {
-	if (firstChainRun == true && b == false) {
-		log("", true);
-		document.getElementById("log").innerHTML = currentDesc.getElementsByTagName("startAA")[1].childNodes[0].nodeValue;
-		dci.style.display = "";
-		b = true;
-		btnA.style.display = "none";
-		btnB.style.display = "none";
-		btnC.style.display = "none";
-		btnD.style.display = "none";
-		document.getElementById("choiceA").innerHTML = "";
-		document.getElementById("choiceB").innerHTML = "";
-		document.getElementById("choiceC").innerHTML = "";
-		document.getElementById("choiceD").innerHTML = "";
-
-	}
-
-};
-function changeForC() {
-	if (firstChainRun == true && c == false) {
-		log(startarea.answerSetStart[1] + "<br>", true);
-		dci.style.display = "";
-		c = true;
-		btnA.style.display = "none";
-		btnB.style.display = "none";
-		btnC.style.display = "none";
-		btnD.style.display = "none";
-	}
-}
-
-function changeForD() {
+/*function changeForD() {
 	if (firstChainRun == true && d == false) {
 		//log(startarea.answerSetStart[2] + "<br>", true);
 		dci.style.display = "";
@@ -292,94 +194,34 @@ function changeForD() {
 		d = true;
 	}
 }
+*/
 
-//this is useless after getting to the second chain!
+
 function continueBASE() {
-	if (a == true) {
-		a = false;
-	}
-	if (b == true) {
-		Swamp1();
-		b = false;
-	}
-	if (c == true) {
-		secondChain();
-		c = false;
-	}
-	if (d == true) {
-		secondChain();
-		d = false;
-	}
+
 }
+
 
 function continuer() {
-	if (a == true) {
-		continuerChain();
-		a = false;
-	}
-	if (b == true) {
-		continuerChain();
-		b = false;
-	}
-	if (c == true) {
-		continuerChain();
-		c = false;
-	}
-	if (d == true) {
-		continuerChain();
-		d = false;
-	}
-	if (Swp1 == true && swordget == true) {
-		Swamp1();
-	}
 }
+
 
 function continuerChain() {
-	if ( Swamp1 = true) {
-		if ( a = true) {
-			btnA.style.display = "none";
-			btnB.style.display = "none";
-			btnC.style.display = "none";
-			btnD.style.display = "none";
-		}
-	}
-}
-
-function Swamp1() {
-	Swp1 = true;
-	firstChainRun = false;
-	$("#choiceA");
-	document.getElementById("choiceA").innerHTML = currentDesc.getElementsByTagName("swampOneA")[0].childNodes[0].nodeValue + "<br>";
-	document.getElementById("choiceB").innerHTML = currentDesc.getElementsByTagName("swampOneA")[1].childNodes[0].nodeValue + "<br>";
-	document.getElementById("choiceC").innerHTML = currentDesc.getElementsByTagName("swampOneA")[2].childNodes[0].nodeValue + "<br>";
-	document.getElementById("choiceD").innerHTML = currentDesc.getElementsByTagName("swampOneA")[3].childNodes[0].nodeValue + "<br>";
-	document.getElementById("log").innerHTML = currentDesc.getElementsByTagName("swampOneOpDesc")[0].childNodes[0].nodeValue + "<br>";
-	endurance = endurance + 1;
-	document.getElementById("end").innerHTML = endurance;
-	dci.style.display = "none";
-
-	btnA.style.display = "";
-	btnC.style.display = "";
-	btnB.style.display = "";
-	btnD.style.display = "";
-	function dosomething() {
-		if (swordget == true) {
-			document.getElementById("choiceA").innerHTML = currentDesc.getElementsByTagName("swampOneA")[1].childNodes[0].nodeValue + "<br>";
-			document.getElementById("choiceB").innerHTML = currentDesc.getElementsByTagName("swampOneA")[2].childNodes[0].nodeValue + "<br>";
-			document.getElementById("choiceC").innerHTML = currentDesc.getElementsByTagName("swampOneA")[3].childNodes[0].nodeValue + "<br>";
-			document.getElementById("choiceD").innerHTML = "";
-			document.getElementById("log").innerHTML = currentDesc.getElementsByTagName("swampOneOpDescS")[0].childNodes[0].nodeValue + "<br>";
-			btnD.style.display = "none";
-			nami.style.display = "none";
-			Inventory.set(InvKeyString, "Runed Bastard-Sword");
-			intellegence = intellegence - 1;
-			document.getElementById("int").innerHTML = intellegence;
-		}
-	}
-
-	dosomething();
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //COMBAT MODULE START!\\
 
